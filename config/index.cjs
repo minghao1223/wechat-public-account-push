eb1b6c49f79f77a517d32f6ddcdcdbd622e69d18
minghao1223 <57787114+minghao1223@users.noreply.github.com>
2022-10-06 22:54:02 +0900

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,7 +32,36 @@ const USER_CONFIG = {
           type: '生日', name: '多多', year: '1997', date: '03-17',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2011', date: '04-01',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+  ],
+  
+    USERS: [
+    {
+      // 想要发送的人的名字
+      name: 'Qy',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ogm0j6fFTXMGH5N4qZtq3x0q4bGE',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'qo1ZuTW7_r7lGaFQoINkvs26MIROD76FNRpzgwRXrJM',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-17',
+      festivals: [
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '多多', year: '1997', date: '03-17',
+        },
+        {
+          type: '节日', name: '恋爱纪念日', year: '2011', date: '04-01',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
