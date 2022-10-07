3a4751b2f9b90180ca0ea358968c80de9858acc9
minghao1223 <57787114+minghao1223@users.noreply.github.com>
2022-10-07 10:41:34 +0900

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'FTVxiC5oI77RRcHqmdOoJQLbH6TqfcBbdPY8Zoh3zBw',
 
   CALLBACK_USERS: [
     {
